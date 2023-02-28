6bfa2b866a3e53d0375531323782c2b02a203fb2
Natalya <yara.yarina@mail.ru>
2023-02-28 21:19:10 +0300

    Сохранение решения

--- Homework_JS_4/lesson4.js
+++ Homework_JS_4/lesson4.js
@@ -32,7 +32,29 @@ console.log(arr);
 // 2. Найти минимальное число
 // 3. Найти есть ли в этом массиве число 3
 
-const random = Math.random()
+let x = Math.ceil(Math.random()*10);
+let y = Math.ceil(Math.random()*10);
+let a = Math.ceil(Math.random()*10);
+let b = Math.ceil(Math.random()*10);
+let c = Math.ceil(Math.random()*10);
+const random = [x, y, a, b, c];
+console.log(random);
+
+let sum = random.map(i=>x+=i, x=0).reverse()[0]
+console.log(sum);
+
+let min = Math.min(...random);
+console.log(min);
+
+function find(random) {
+    for (let i = 0; i < random.length; i++) {
+        if (random[i] === 3) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(find(random))
 
 // *Необязательное задание. *
 // Необходимо вывести горку в консоль (используя цикл for), как показано на рисунке, только у вашей горки должно быть 20 рядов, а не 5:
