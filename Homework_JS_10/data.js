776323e19157fb5af5e24f8dcc7fcee702638aa5
Natalya <yara.yarina@mail.ru>
2023-03-10 01:57:46 +0300

    Сохранение

---
+++ Homework_JS_10/data.js
@@ -0,0 +1,38 @@
+const dataInfo =  `[
+    {
+      "API": "weather-api",
+      "Description": "A RESTful free API to check the weather",
+      "Auth": "",
+      "HTTPS": true,
+      "Cors": "no",
+      "Link": "https://github.com/robertoduessmann/weather-api",
+      "Category": "Weather"
+    },
+    {
+      "API": "WeatherAPI",
+      "Description": "Weather API with other stuff like Astronomy and Geolocation API",
+      "Auth": "apiKey",
+      "HTTPS": true,
+      "Cors": "yes",
+      "Link": "https://www.weatherapi.com/",
+      "Category": "Weather"
+    },
+    {
+      "API": "Weatherbit",
+      "Description": "Weather",
+      "Auth": "apiKey",
+      "HTTPS": true,
+      "Cors": "unknown",
+      "Link": "https://www.weatherbit.io/api",
+      "Category": "Weather"
+    },
+    {
+      "API": "Yandex.Weather",
+      "Description": "Assesses weather condition in specific locations",
+      "Auth": "apiKey",
+      "HTTPS": true,
+      "Cors": "no",
+      "Link": "https://yandex.com/dev/weather/",
+      "Category": "Weather"
+    }
+]`
