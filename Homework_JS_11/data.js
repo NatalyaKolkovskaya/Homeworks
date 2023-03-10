20b91ee30915e115e31e0742af7c8b896f9f814c
Natalya <yara.yarina@mail.ru>
2023-03-11 00:22:40 +0300

    Сохранение проекта

---
+++ Homework_JS_11/data.js
@@ -0,0 +1,26 @@
+const dataProducts = `[
+    {
+        "img": "img/item1.svg",
+        "nameProduct": "MANGO PEOPLE T-SHIRT",
+        "price": "$300",
+        "color": "Red",
+        "size": "Xl",
+        "quantity": 2
+     },
+     {
+        "img": "img/item2.svg",
+        "nameProduct": "ELLERY X M'O CAPSULE",
+        "price": "$52",
+        "color": "Black",
+        "size": "Xl",
+        "quantity": 1
+     },
+     {
+        "img": "img/item2.svg",
+        "nameProduct": "ELLERY X M'O CAPSULE",
+        "price": "$52",
+        "color": "Black",
+        "size": "Xl",
+        "quantity": 1
+     }
+]`
